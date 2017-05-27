32cdd77827661fbcd2b1e791fb576d5d8985fcce
Frederic <frederic.fayet@gmail.com>
2017-05-27 20:46:05 +0200

    Update for title on about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -4,7 +4,7 @@ var About = React.createClass({
     render: function() {
         return (
             <div>
-                <h3>Mon composant ABOUT !</h3>
+                <h3>ABOUT</h3>
                 <p>Bienvenu sur la page about !</p>
             </div>
         );
