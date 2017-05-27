7b654b85d8a0e009ac4ac4c227543f13eb71b906
Frederic <frederic.fayet@gmail.com>
2017-05-27 17:26:13 +0200

    Prep repo for Heroku

--- server.js
+++ server.js
@@ -2,9 +2,22 @@ var express = require('express');
 
 //On crée notre app :
 var app = express();
+const PORT = process.env.PORT || 3000;
+
+//Permet de rediriger le HTTPS vers HTTP :
+app.use(function(req, res, next) {
+  if (req.headers['x-forwarded-proto'] === 'http')
+  {
+    next();
+  }
+  else
+  {
+    res.redirect('http://' + req.hostname + req.url);
+  }
+});
 
 app.use(express.static('public'));
 
 app.listen(3000, function() {
-  console.log("Serveur is running on port 3000 !");
+  console.log("Serveur is running on port " + PORT + " !");
 });
